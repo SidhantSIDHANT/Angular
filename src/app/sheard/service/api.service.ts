b8a1f89d85349ba6552fced219be2238872fa4d5
siddhantsidd <122714110+siddhantsidd@users.noreply.github.com>
2024-03-21 21:03:42 +0530

    changes are working

--- src/app/sheard/service/api.service.ts
+++ src/app/sheard/service/api.service.ts
@@ -9,13 +9,14 @@ import { Observable } from "rxjs";
 export class ApiService {
   url: string = `https://restcountries.com/v3.1/all`;
 
-  days: any = [
+  days: Array<{ days: string, id: number }> = [
     { days: "7-15 days", id: 1, },
     { days: "15-30 days", id: 2 },
     { days: "35-45 days", id: 7 },
     { days: "45-60 days", id: 3 },
     { days: "60-90 days", id: 6 },
   ]
+  
   constructor(private _http: HttpClient) { }
 
   getCountry(): Observable<any> {
